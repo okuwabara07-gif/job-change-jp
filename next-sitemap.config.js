417076146ae9618d37f33a907af5063d971aa2ff
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-05 16:23:56 +0900

    feat: add next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://job-change-jp.vercel.app',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://job-change-jp.vercel.app/sitemap.xml'],
+  },
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
